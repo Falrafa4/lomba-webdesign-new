2f81e9b930511b56e6b96e70471c7575150625b7
Lidiman <92214434+Lidiman@users.noreply.github.com>
2024-09-15 06:13:33 +0700

    Kasih Javascript

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 window.addEventListener('scroll', function() {
-    const galleryItems = document.querySelectorAll('.gallery-item');
-  
-    galleryItems.forEach(item => {
-      const itemTop = item.getBoundingClientRect().top;
-      const windowHeight = window.innerHeight;
-  
-      if (itemTop < windowHeight * 0.8) { 
-        item.classList.add('show');
-      } else {
-        item.classList.remove('show');
-      }
-    });
+  const galleryItems = document.querySelectorAll('.gallery-item');
+
+  galleryItems.forEach(item => {
+    const itemTop = item.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (itemTop < windowHeight * 0.8) { 
+      item.classList.add('show');
+    } else {
+      item.classList.remove('show');
+    }
   });
-  
+});
