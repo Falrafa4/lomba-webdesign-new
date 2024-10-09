b56d5f0ae576a64ec3b75e500cbc3000ce5238bc
Xavier0010 <amadeus.xavier.enoch@gmail.com>
2024-10-09 15:09:06 +0700

    Minor changes.

--- contact/main.js
+++ contact/main.js
@@ -1,24 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const toggleSidebar = document.querySelector('.toggleSidebar');
-    const sidebar = document.querySelector('.sidebar');
-    const closeBtn = document.querySelector('.close-btn');
-  
-    toggleSidebar.addEventListener('click', () => {
-        sidebar.classList.toggle('open');
-    });
-  
-    closeBtn.addEventListener('click', () => {
-        sidebar.classList.remove('open');
-    });
+  const toggleSidebar = document.querySelector('.toggleSidebar');
+  const sidebar = document.querySelector('.sidebar');
+  const closeBtn = document.querySelector('.close-btn');
+
+  toggleSidebar.addEventListener('click', () => {
+      sidebar.classList.toggle('open');
   });
+
+  closeBtn.addEventListener('click', () => {
+      sidebar.classList.remove('open');
+  });
+});
 
 var prevScrollpos = window.scrollY;
 window.onscroll = function() {
   var currentScrollPos = window.scrollY;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-10vh";
+  if (currentScrollPos === 0) {
+    document.getElementById('navbar').style.top = '0';
   }
+  if (prevScrollpos < currentScrollPos) {
+    document.getElementById('navbar').style.top = '-7vh';
+  }
+
   prevScrollpos = currentScrollPos;
 }
--- main.js
+++ main.js
@@ -15,10 +15,12 @@ document.addEventListener('DOMContentLoaded', function() {
 var prevScrollpos = window.scrollY;
 window.onscroll = function() {
   var currentScrollPos = window.scrollY;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-10vh";
+  if (currentScrollPos === 0) {
+    document.getElementById('navbar').style.top = '0';
   }
+  if (prevScrollpos < currentScrollPos) {
+    document.getElementById('navbar').style.top = '-7vh';
+  }
+
   prevScrollpos = currentScrollPos;
 }
